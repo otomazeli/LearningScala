5505c555c72b45fc9eaeed057de961b85859231f
Dan <dan.dixey@gmail.com>
2017-11-11 18:07:41 +0000

    Updating the Script

--- src/main/scala/MathsJam/MathsJamTweet.scala
+++ src/main/scala/MathsJam/MathsJamTweet.scala
@@ -3,41 +3,25 @@ package MathsJam
 object MathsJamTweet {
   // Original Tweet: https://twitter.com/tombutton/status/929358013990866945
 
-  /** Check if a value is a Prime Number
-    *
-    * @param n  Value to Check
-    * @return   Boolean
-    */
-  def isPrimeForBigInt(n: BigInt): Boolean = {
+  def isPrime(n: Int): Boolean = !Range(2, n - 1).exists(n % _ == 0)
 
-    def ceiling: BigInt = {
-      def f(a: BigInt): Stream[BigInt] = a #:: f(a + 1)
-      f(BigInt(1)).dropWhile(_.pow(2) < n)(0)
-    }
-    Range.BigInt(BigInt(2), ceiling, BigInt(1)) forall (x => n % x != 0)
+  def simpleCheck(k: Int): Boolean = {
+    isPrime(k) & isPrime((k + 2) / 3) & isPrime((k + 1) / 2)
   }
 
-  /** Check the Combinations as described in the Tweet
-    *
-    * @param x
-    * @return
-    */
-  def checkProblem(x: BigInt): Boolean = {
-    x match {
-      case k
-          if isPrimeForBigInt(k) & isPrimeForBigInt((k + 2) / 3) &
-            isPrimeForBigInt((k + 1) / 2) => // & isPrimeForBigInt((k + 3) / 4)
-        true
-      case _ => false
-    }
+  def simpleCheck2(k: Int): Boolean = {
+    isPrime(k) & isPrime((k + 2) / 3) & isPrime((k + 1) / 2) & isPrime((k + 3) / 4)
   }
 
   def main(args: Array[String]): Unit = {
     // Specify a range to check
-    val rangeValues = Stream.range[BigInt](BigInt(2018), BigInt(999999999999999999L))
+    val rangeValues = Stream.range[Int](2018, 12722)
 
     // Display the result
-    println(s"First value: ${rangeValues.takeWhile(checkProblem)}")
+    println(s"Next value            => ${rangeValues.filter(simpleCheck).toList.head}")
+    // Next value            : 2557
+    println(s"Next value (extended) => ${rangeValues.filter(simpleCheck2).toList.head}")
+    // Next value (extended) : 12721
   }
 
 }
