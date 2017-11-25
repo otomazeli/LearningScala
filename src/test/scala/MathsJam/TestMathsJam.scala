f4a0a31483ded2e844df1be2f5f6fb1eaab12aa8
Dan Dixey <dan.dixey@gmail.com>
2017-11-25 13:45:05 +0000

    Maths jam tweet (#12)
    * MathJam question
    
    * Updating the Script
    
    * Adding a Unit Test
    
    * Updating the Project dependencies
    
    * Updating the use of Head to HeadOption

--- src/test/scala/MathsJam/TestMathsJam.scala
+++ src/test/scala/MathsJam/TestMathsJam.scala
@@ -7,7 +7,7 @@ class TestMathsJam extends FlatSpec with Matchers {
 
   "MathsJam question" should "prime finder" in {
     val rangeValues = Stream.range[Int](2018, 12722)
-    assert(rangeValues.filter(simpleCheck).toList.head == 2557)
-    assert(rangeValues.filter(simpleCheck2).toList.head == 12721)
+    assert(rangeValues.filter(simpleCheck).toList.headOption.contains(2557))
+    assert(rangeValues.filter(simpleCheck2).toList.headOption.contains(12721))
   }
 }
